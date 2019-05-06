abd8237c8e77b6ce0bf665404ebd3bb0679d1abd
theNerd247 <noah.harvey247 gm>
2019-05-05 22:18:20 -0400

    adds makeIndex to build system

--- sermons.nix
+++ sermons.nix
@@ -11,11 +11,13 @@ stdenv.mkDerivation {
       for f in $(ls ./mvc/*.md); do
         ${pandoc}/bin/pandoc -s -o docs/mvc/$(basename -s.md $f).pdf $f
       done
+      ./makeIndex
     '';
 
     installPhase = 
       ''
         mkdir -p $out
         cp -r docs/ $out/docs
+        cp index.html $out/index.html
       '';
 }
